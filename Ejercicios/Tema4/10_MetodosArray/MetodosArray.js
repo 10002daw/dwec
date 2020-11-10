39d7a3ca64e7c511089fd430e5443877a168a41e
Eduardo Jiménez Bahíllo <10002@iespuertasdelcampo.es>
2020-11-10 11:54:05 +0100

    2020-11-10 Clase

--- Ejercicios/Tema4/10_MetodosArray/MetodosArray.js
+++ Ejercicios/Tema4/10_MetodosArray/MetodosArray.js
@@ -3,3 +3,22 @@ num2 = ["cinco","seis","siete"];
 
 $1 = num1.concat(num2);
 console.log($1);
+
+$1.pop();
+$1.shift();
+console.log($1);
+
+$1.unshift("primero")
+$1.push("otro");
+console.log($1);
+
+for ( let i in $1 ) {
+    if ( !isNaN($1[i]) ) {
+        $1[i] = Math.trunc($1[i]);
+    }
+}
+console.log($1);
+
+console.log($1.join(" y "));
+
+console.log($1.slice(1,4).join(" y "));
