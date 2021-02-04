c39c1cec1c04637c46c4dc6138e3e07cbc844054
Eduardo Jiménez Bahíllo <10002@iespuertasdelcampo.es>
2021-02-04 12:46:32 +0100

    dasf

--- Prácticas/Eva2/Practica8/script.js
+++ Prácticas/Eva2/Practica8/script.js
@@ -1,14 +1,18 @@
+//Creamos el cartel, que será un elemento div
 cartel = document.createElement("div");
 
+//Le aplicamos todas las reglas CSS para que se muestre como queramos
 cartel.innerHTML = "Hola caracola";
 cartel.style.backgroundColor = "yellow";
 cartel.style.position = "fixed";
-cartel.style.width = "100px";
-cartel.style.height = "80px";
-cartel.style.top = "0";
-cartel.style.left = "0";
+cartel.style.padding = "20px";
+cartel.style.border = "solid 1px black";
+
+//Insertamos el cartel en el documento
 document.body.appendChild(cartel);
 
+
+//Evento que se produce cuando se mueve el ratón, que cambia la posición del cartel a la del puntero del ratón
 document.addEventListener("mousemove", function(ev) {
     cartel.style.top = `${ev.clientY}px`;
     cartel.style.left = `${ev.clientX}px`;
